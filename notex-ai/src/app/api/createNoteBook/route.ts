6bc02c5e86b16fa86cb71384399ec56dfecae7c3
neeraj-gs <gsneeraj05@gmail.com>
2023-10-31 19:37:25 +0530

    Added react-query to connect frotend and backend and also allows us to manage state within our apllication to hit end points to mutate and get back teh results

--- notex-ai/src/app/api/createNoteBook/route.ts
+++ notex-ai/src/app/api/createNoteBook/route.ts
@@ -1,6 +1,7 @@
 //it is a special file that routes to the file folder strucutere and map to teh rest api point
 // this represetns /api/createNoteBook
 
+import { generateImagePrompt } from "@/lib/openai";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
@@ -12,5 +13,9 @@ export async function POST(req:Request){
 
     const body = await req.json();
     const {name} = body;
+
+    const image_description = await generateImagePrompt(name);
+    console.log(image_description);
+    return new NextResponse("OK",{status:200})
 }
 
