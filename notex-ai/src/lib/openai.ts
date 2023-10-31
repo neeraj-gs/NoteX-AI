3a13065e11da6218a944c5bc9866602e30c584e3
neeraj-gs <gsneeraj05@gmail.com>
2023-10-31 19:21:57 +0530

    Created a openai file that takes the description and then started with creting image from dalle api

---
+++ notex-ai/src/lib/openai.ts
@@ -0,0 +1,10 @@
+export async function  generateImagePrompt(prompt:string){
+    //takes a name and geneerates a image or a thumbnail 
+    // this fucnitn is responsible for using open ai api and generate a descriptive text for a name notebook 
+    
+
+}
+
+export async function generateImage(){
+
+}
