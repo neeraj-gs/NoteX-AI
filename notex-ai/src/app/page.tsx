e9575ee5743052924ac0a5fd222e610e640afae8
neeraj-gs <gsneeraj05@gmail.com>
2023-10-31 09:34:35 +0530

    ADded CLerk for authentication

--- notex-ai/src/app/page.tsx
+++ notex-ai/src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
+    <>
     <div className='text-3xl text-red-500'>NoteX</div>
+    <Button variant="destructive">SHad BTN</Button>
+    </>
   )
 }
