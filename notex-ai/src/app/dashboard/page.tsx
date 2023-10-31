c84aa5339cf90de32fa76c58f89d2c8e6d8af413
neeraj-gs <gsneeraj05@gmail.com>
2023-10-31 12:03:39 +0530

    Added Dashboard Page , Need to fix the BUg of Clerk AUthentication to show on Dashboard page not initially

---
+++ notex-ai/src/app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+type Props = {}
+
+const DashboardPage = (props: Props) => {
+  return (
+    <>
+        <div className='grainy min-h-screen'>
+            <div className='max-w-7xl mx-auto p-10'>
+                <div className="h-14"></div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default DashboardPage
--- notex-ai/src/app/page.tsx
+++ notex-ai/src/app/page.tsx
@@ -3,15 +3,21 @@ import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import Link from 'next/link'
 import { ArrowRight } from 'lucide-react'
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <div className='bg-gradient-to-r min-h-screen grainy from-rose-100 to-teal-100'>
       <div className='flex justify-between'>
         <h2 className='text-3xl text-green-600 font-extrabold'>NoteX AI</h2>
-        <div>
-          <Button  className='mr-2 bg-green-600'>Sign Up</Button>
-          <Button  className='mr-2 bg-green-600'>Sign In</Button>
+        <div className='mr-4 mt-2'>
+          {/* <Link href="/sign-up">
+            <Button  className='mr-2 bg-green-600'>Sign Up</Button>
+          </Link>
+          <Link href="/sign-in">
+            <Button  className='mr-2 bg-green-600'>Sign In</Button>
+          </Link> */}
+          <UserButton afterSignOutUrl='/'/>
         </div>
       </div>
       
