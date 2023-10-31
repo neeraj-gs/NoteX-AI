e99973418d3c241c1546672e61850ce260d55eb8
neeraj-gs <gsneeraj05@gmail.com>
2023-10-31 19:23:06 +0530

    install openai-edge hat is a special version of openai that allows to run on each vercel runtime to run faster and longer

--- notex-ai/src/lib/openai.ts
+++ notex-ai/src/lib/openai.ts
@@ -1,7 +1,10 @@
 export async function  generateImagePrompt(prompt:string){
     //takes a name and geneerates a image or a thumbnail 
     // this fucnitn is responsible for using open ai api and generate a descriptive text for a name notebook 
+
+    //this is given to the other function to generate a detail description iamge
     
+
 
 }
 
