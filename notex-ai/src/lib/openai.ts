f2ffa7d1bb1926bd6a2eed2cd1df522f3c3e124b
neeraj-gs <gsneeraj05@gmail.com>
2023-10-31 19:35:01 +0530

    Added a fucntion taht will genereate a desctiption based on anme

--- notex-ai/src/lib/openai.ts
+++ notex-ai/src/lib/openai.ts
@@ -1,9 +1,37 @@
-export async function  generateImagePrompt(prompt:string){
+import {Configuration,OpenAIApi} from  'openai-edge'
+
+const config =  new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(config)
+
+
+export async function  generateImagePrompt(name:string){
     //takes a name and geneerates a image or a thumbnail 
     // this fucnitn is responsible for using open ai api and generate a descriptive text for a name notebook 
 
     //this is given to the other function to generate a detail description iamge
-    
+
+    try{
+        const response = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages:[{
+                role:'system',
+                content:'You Are a Creative and Helpful AP Assistant capable  of generating interesting thumbnail descriptions for my notes. Your Output will be Fed to the DALLE API to generaate a thumbnail. The desription should be minimalistic and flat styled'
+            },{
+                role: 'user',
+                content: `Please Generate a Thumbnaol description for my notebook title ${name}`
+            }]
+        })
+        const data = await response.json();
+        const image_description = data.choices[0].message.content;
+        return image_description as string;
+        
+    }catch(error){
+        console.log(error);
+        throw error;
+    }
 
 
 }
