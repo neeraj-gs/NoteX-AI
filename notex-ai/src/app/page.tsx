7b887853c3805fca97301fd7ce1c1efb898e9e7b
neeraj-gs <gsneeraj05@gmail.com>
2023-10-31 10:51:56 +0530

    Added the Landing page added a gradient and made it bluish

--- notex-ai/src/app/page.tsx
+++ notex-ai/src/app/page.tsx
@@ -3,9 +3,11 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <>
-    <div className='text-3xl text-red-500'>NoteX</div>
-    <Button variant="destructive">SHad BTN</Button>
-    </>
+    <div className='bg-gradient-to-r min-h-screen grainy from-rose-100 to-teal-100'>
+      <h2>NoteX AI.</h2>
+      <div className='h-screen flex justify-center items-center'>
+        <h1 className='font-semibold text-4xl text-center'>AI Notes App Assisted with Your AI Companion.</h1>
+      </div>
+    </div>
   )
 }
