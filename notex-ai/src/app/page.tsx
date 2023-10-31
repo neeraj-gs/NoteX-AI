a6ad1d50b29cd326a5aac6fcd9e774c3d21883a2
neeraj-gs <gsneeraj05@gmail.com>
2023-10-31 11:54:30 +0530

    Added SIgn upa dn sign in on the main landing page

--- notex-ai/src/app/page.tsx
+++ notex-ai/src/app/page.tsx
@@ -1,11 +1,20 @@
 import TypeWriterTitle from '@/components/TypeWriterTitle'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import Link from 'next/link'
+import { ArrowRight } from 'lucide-react'
 
 export default function Home() {
   return (
     <div className='bg-gradient-to-r min-h-screen grainy from-rose-100 to-teal-100'>
-      <h2>NoteX AI.</h2>
+      <div className='flex justify-between'>
+        <h2 className='text-3xl text-green-600 font-extrabold'>NoteX AI</h2>
+        <div>
+          <Button  className='mr-2 bg-green-600'>Sign Up</Button>
+          <Button  className='mr-2 bg-green-600'>Sign In</Button>
+        </div>
+      </div>
+      
       <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
         <h1 className='font-semibold text-4xl text-center'>
           Notes App Integrated with your 
@@ -17,6 +26,12 @@ export default function Home() {
         <h3 className='font-semibold text-3xl text-center text-slate-700'>
           <TypeWriterTitle />
         </h3>
+        <div className="mt-8"></div>
+
+        <Link href="/dashboard" className='flex justify-center'>
+          <Button className='bg-green-600'>Get Started <ArrowRight className='ml-2 w-5 h-5' /></Button>
+        </Link>
+
       </div>
     </div>
   )
