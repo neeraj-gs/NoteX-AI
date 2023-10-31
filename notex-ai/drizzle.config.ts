d3a560121462c001d6067464feac2dd64842535a
neeraj-gs <gsneeraj05@gmail.com>
2023-10-31 18:42:27 +0530

    Addded drizzle kit and also changed es6 in tsconfig

--- notex-ai/drizzle.config.ts
+++ notex-ai/drizzle.config.ts
@@ -1,5 +1,11 @@
 //this is where we specify schema configuration for drizzle kit
 import type { Config } from "drizzle-kit"; //tell us what config is needed
+import * as dotenv from 'dotenv'
+
+dotenv.config({
+    path: '.env' //just tells the path so taht drizle gets acces to the .env file
+})
+
 
 export default {
     driver: 'pg',
