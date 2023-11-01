5fd035576e6152140d6dfa8ea114daa2f910ea3e
neeraj-gs <gsneeraj05@gmail.com>
2023-11-01 11:24:10 +0530

    Added a Bold button and triigerde toggle bold

--- notex-ai/src/components/TipTapEditor.tsx
+++ notex-ai/src/components/TipTapEditor.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import {EditorContent, useEditor} from '@tiptap/react'
 import {StarterKit} from '@tiptap/starter-kit'
+import TipTapMenuBar from './TipTapMenuBar'
+import { Button } from './ui/button'
 
 type Props = {}
 
@@ -17,11 +19,19 @@ const TipTapEditor = (props: Props) => {
         }
     })
   return (
-    <div>
+    <>
+        <div className='flex'>
+            {editor && (
+                <TipTapMenuBar editor={editor} />
+            )}
+            
+            <Button>Saved</Button>
+        </div>
+
         <div>
             <EditorContent editor={editor} />
         </div>
-    </div>
+    </>
   )
 }
 
