a14403d9a1eae1e834e85a9e4f3f692280c8f600
neeraj-gs <gsneeraj05@gmail.com>
2023-11-01 20:04:14 +0530

    Converted png to ico file and added it ot the favicon

--- notex-ai/src/app/layout.tsx
+++ notex-ai/src/app/layout.tsx
@@ -19,10 +19,6 @@ export default function RootLayout({
   return (
     
       <html lang="en">
-        <head>
-        {/* Add the favicon link tag here */}
-        <link rel="icon" href="./notes.png" />
-      </head>
         <ClerkProvider>
           <Provider>
           <body className={inter.className}>{children}</body>
