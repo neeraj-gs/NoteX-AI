9a54b5dc4faf39d2f582ff30fc818d6b67c1de9b
neeraj-gs <gsneeraj05@gmail.com>
2023-11-01 18:57:31 +0530

    remoced the big of editor visible only tot the top in the note editor page

--- notex-ai/src/app/notebook/[noteId]/page.tsx
+++ notex-ai/src/app/notebook/[noteId]/page.tsx
@@ -1,62 +1,64 @@
-import TipTapEditor from '@/components/TipTapEditor'
-import { Button } from '@/components/ui/button'
-import { clerk } from '@/lib/clerk-server'
-import { db } from '@/lib/db'
-import { $notes } from '@/lib/db/schema'
-import { auth } from '@clerk/nextjs'
-import { and, eq } from 'drizzle-orm'
-import { ArrowLeft } from 'lucide-react'
-import Link from 'next/link'
-import { redirect } from 'next/navigation'
-import React from 'react'
+// import DeleteButton from "@/components/DeleteButton";
+import TipTapEditor from "@/components/TipTapEditor";
+import { Button } from "@/components/ui/button";
+import { clerk } from "@/lib/clerk-server";
+import { db } from "@/lib/db";
+import { $notes } from "@/lib/db/schema";
+import { auth } from "@clerk/nextjs";
+import { and, eq } from "drizzle-orm";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import React from "react";
 
 type Props = {
-    params:{
-        noteId:string
-    }
-}
+  params: {
+    noteId: string;
+  };
+};
 
-const NotebookPage = async ({params:{noteId}}: Props) => {
-    const {userId} = await auth();
-    if(!userId){
-        return redirect('/dashboard')
-    }
-    const user = await clerk.users.getUser(userId)
-    const notes = await db
+const NotebookPage = async ({ params: { noteId } }: Props) => {
+  const { userId } = await auth();
+  if (!userId) {
+    return redirect("/dashboard");
+  }
+  const user = await clerk.users.getUser(userId);
+  const notes = await db
     .select()
     .from($notes)
     .where(and(eq($notes.id, parseInt(noteId)), eq($notes.userId, userId)));
 
-    if(!notes.length){
-        return redirect('/dashboard')
-    }
-
-    const note = notes[0];
-
+  if (notes.length != 1) {
+    return redirect("/dashboard");
+  }
+  const note = notes[0];
 
   return (
-    <div className='min-h-screen grainy p-8'>
-        <div className='max-w-4xl mx-auto'>
-            <div className='border shadow-xl border-stone-200 rounded-xl p-4 flex items-center'>
-                <Link href='/dashboard'>
-                    <Button className='bg-green-600' size='sm'><ArrowLeft /> Back</Button>
-                </Link>
-                <div className="w-4"></div>
-                <span className='font-semibold'>
-                    {user.firstName} {user.lastName}
-                </span>
-                <span className='inline-block mx-1'>/</span>
-                <span className='text-stone-500 font-semibold'>{note.name}</span>
-                <div className="ml-auto">DELETE</div>
-            </div>
-
-            <div className="h-4"></div>
-            <div className='border-stone-200 shadow-xl rounded-lg px-16 py-8 w-full '></div>
-            {/* Editor with all auto functionalities */}
-            <TipTapEditor note={note} />
+    <div className="min-h-screen grainy p-8">
+      <div className="max-w-4xl mx-auto">
+        <div className="border shadow-xl border-stone-200 rounded-lg p-4 flex items-center">
+          <Link href="/dashboard">
+            <Button className="bg-green-600" size="sm">
+              Back
+            </Button>
+          </Link>
+          <div className="w-3"></div>
+          <span className="font-semibold">
+            {user.firstName} {user.lastName}
+          </span>
+          <span className="inline-block mx-1">/</span>
+          <span className="text-stone-500 font-semibold">{note.name}</span>
+          <div className="ml-auto">
+            {/* <DeleteButton noteId={note.id} /> */}
+          </div>
         </div>
-    </div>
-  )
-}
 
-export default NotebookPage
+        <div className="h-4"></div>
+        <div className="border-stone-200 shadow-xl border rounded-lg px-16 py-8 w-full">
+          <TipTapEditor note={note} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotebookPage;
