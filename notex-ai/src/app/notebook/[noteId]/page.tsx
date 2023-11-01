f495480bd05686f43bd6c8b3114faffcbd5bc150
neeraj-gs <gsneeraj05@gmail.com>
2023-11-01 11:16:04 +0530

    Added a Basic Tip Tap editor skeleton and need to customize it better

--- notex-ai/src/app/notebook/[noteId]/page.tsx
+++ notex-ai/src/app/notebook/[noteId]/page.tsx
@@ -1,3 +1,4 @@
+import TipTapEditor from '@/components/TipTapEditor'
 import { Button } from '@/components/ui/button'
 import { clerk } from '@/lib/clerk-server'
 import { db } from '@/lib/db'
@@ -52,6 +53,7 @@ const NotebookPage = async ({params:{noteId}}: Props) => {
             <div className="h-4"></div>
             <div className='border-stone-200 shadow-xl rounded-lg px-16 py-8 w-full '></div>
             {/* Editor with all auto functionalities */}
+            <TipTapEditor />
         </div>
     </div>
   )
--- notex-ai/src/components/TipTapEditor.tsx
+++ notex-ai/src/components/TipTapEditor.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {EditorContent, useEditor} from '@tiptap/react'
+import {StarterKit} from '@tiptap/starter-kit'
 
 type Props = {}
 
 const TipTapEditor = (props: Props) => {
+    const [editorState,setEditorState] =  useState('');
+    const editor = useEditor({
+        autofocus:true,
+        extensions: [
+            StarterKit,
+        ],
+        content:editorState,
+        onUpdate:({editor})=>{
+            setEditorState(editor.getHTML()) //allows to get back html and bind it ot the state
+        }
+    })
   return (
-    <div>TipTapEditor</div>
+    <div>
+        <div>
+            <EditorContent editor={editor} />
+        </div>
+    </div>
   )
 }
 
