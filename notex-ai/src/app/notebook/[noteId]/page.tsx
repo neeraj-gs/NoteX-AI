be1b4a9f6567ac9f5da1267c9f152514ef5ec0cd
neeraj-gs <gsneeraj05@gmail.com>
2023-11-01 11:09:15 +0530

    Added a skeleton on the editor need to build a tip tac editor

--- notex-ai/src/app/notebook/[noteId]/page.tsx
+++ notex-ai/src/app/notebook/[noteId]/page.tsx
@@ -46,7 +46,12 @@ const NotebookPage = async ({params:{noteId}}: Props) => {
                 </span>
                 <span className='inline-block mx-1'>/</span>
                 <span className='text-stone-500 font-semibold'>{note.name}</span>
+                <div className="ml-auto">DELETE</div>
             </div>
+
+            <div className="h-4"></div>
+            <div className='border-stone-200 shadow-xl rounded-lg px-16 py-8 w-full '></div>
+            {/* Editor with all auto functionalities */}
         </div>
     </div>
   )
