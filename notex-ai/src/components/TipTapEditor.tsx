bbd9d7f20a9d07939c83cccc85bb4a17fc52e636
neeraj-gs <gsneeraj05@gmail.com>
2023-11-01 18:51:42 +0530

    Added the Editor for the AI Autocomplete and made it available with it

--- notex-ai/src/components/TipTapEditor.tsx
+++ notex-ai/src/components/TipTapEditor.tsx
@@ -1,82 +1,99 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-import {EditorContent, useEditor} from '@tiptap/react'
-import {StarterKit} from '@tiptap/starter-kit'
-import TipTapMenuBar from './TipTapMenuBar'
-import { Button } from './ui/button'
-import { useDebounce } from '@/lib/useDebounce'
-import { useMutation } from '@tanstack/react-query'
-import axios from 'axios'
-import { NoteType } from '@/lib/db/schema'
-import Text from '@tiptap/extension-text'
+"use client";
+import React from "react";
+import { EditorContent, useEditor } from "@tiptap/react";
+import { StarterKit } from "@tiptap/starter-kit";
+import TipTapMenuBar from "./TipTapMenuBar";
+import { Button } from "./ui/button";
+import { useDebounce } from "@/lib/useDebounce";
+import { useMutation } from "@tanstack/react-query";
+import Text from "@tiptap/extension-text";
+import axios from "axios";
+import { NoteType } from "@/lib/db/schema";
+import { useCompletion } from "ai/react";
 
-type Props = {note:NoteType}
+type Props = { note: NoteType };
 
-const TipTapEditor = ({note}: Props) => {
-    const [editorState,setEditorState] =  useState(note.editorState || '');
-    const saveNote = useMutation({
-        mutationFn: async () => {
-           const response = await axios.post('/api/saveNote',{
-                noteId: note.id,
-                editorState
-           })
-           return response.data
-        }
-    })
-    const customText = Text.extend({
-        addKeyboardShortcuts(){
-            return {
-                'Shift-a': () => {
-                    console.log('Shift-a ai is triggered')
-                    return true;
-                }
-            }
-        }
-    })
-    const editor = useEditor({
-        autofocus:true,
-        extensions: [
-            StarterKit,
-        ],
-        content:editorState,
-        onUpdate:({editor})=>{
-            setEditorState(editor.getHTML()) //allows to get back html and bind it ot the state
-        }
-    })
+const TipTapEditor = ({ note }: Props) => {
+  const [editorState, setEditorState] = React.useState(
+    note.editorState || `<h1>${note.name}</h1>`
+  );
+  const { complete, completion } = useCompletion({
+    api: "/api/completion",
+  });
+  const saveNote = useMutation({
+    mutationFn: async () => {
+      const response = await axios.post("/api/saveNote", {
+        noteId: note.id,
+        editorState,
+      });
+      return response.data;
+    },
+  });
+  const customText = Text.extend({
+    addKeyboardShortcuts() {
+      return {
+        "Shift-a": () => {
+          // take the last 30 words
+          const prompt = this.editor.getText().split(" ").slice(-30).join(" ");
+          complete(prompt);
+          return true;
+        },
+      };
+    },
+  });
 
-    const debouncedEditorState = useDebounce(editorState,500)
+  const editor = useEditor({
+    autofocus: true,
+    extensions: [StarterKit, customText],
+    content: editorState,
+    onUpdate: ({ editor }) => {
+      setEditorState(editor.getHTML());
+    },
+  });
+  const lastCompletion = React.useRef("");
 
-    useEffect(()=>{
-        //save to db
-        if(debouncedEditorState === ''){
-            return
-        }
-        saveNote.mutate(undefined,{
-            onSuccess:data=>{
-                console.log(data)
-            },
-            onError:error=>{
-                console.log(error)
-            }
-        })
-        
-        console.log(debouncedEditorState)
-    },[debouncedEditorState])
+  React.useEffect(() => {
+    if (!completion || !editor) return;
+    const diff = completion.slice(lastCompletion.current.length);
+    lastCompletion.current = completion;
+    editor.commands.insertContent(diff);
+  }, [completion, editor]);
+
+  const debouncedEditorState = useDebounce(editorState, 500);
+  React.useEffect(() => {
+    // save to db
+    if (debouncedEditorState === "") return;
+    saveNote.mutate(undefined, {
+      onSuccess: (data) => {
+        console.log("success update!", data);
+      },
+      onError: (err) => {
+        console.error(err);
+      },
+    });
+  }, [debouncedEditorState]);
   return (
     <>
-        <div className='flex'>
-            {editor && (
-                <TipTapMenuBar editor={editor} />
-            )}
-            
-            <Button className='ml-4'>Saved</Button>
-        </div>
+      <div className="flex">
+        {editor && <TipTapMenuBar editor={editor} />}
+        <Button disabled variant={"outline"}>
+          Saved
+        </Button>
+      </div>
 
-        <div className='prose'>
-            <EditorContent editor={editor} />
-        </div>
+      <div className="prose prose-sm w-full mt-4">
+        <EditorContent editor={editor} />
+      </div>
+      <div className="h-4"></div>
+      <span className="text-sm">
+        Tip: Press{" "}
+        <kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg">
+          Shift + A
+        </kbd>{" "}
+        for AI autocomplete
+      </span>
     </>
-  )
-}
+  );
+};
 
-export default TipTapEditor
+export default TipTapEditor;
