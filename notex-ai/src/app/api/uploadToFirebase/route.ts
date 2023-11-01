4243ecce76bbe9b74642f027ca99f66f9ad20dfb
neeraj-gs <gsneeraj05@gmail.com>
2023-11-01 19:24:37 +0530

    Added a code for uploading teh firebase_url to the database and storing it permanently

--- notex-ai/src/app/api/uploadToFirebase/route.ts
+++ notex-ai/src/app/api/uploadToFirebase/route.ts
@@ -1,5 +1,6 @@
 import { db } from "@/lib/db";
 import { $notes } from "@/lib/db/schema";
+import { uploadFileToFireBase } from "@/lib/firebase";
 import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
@@ -14,7 +15,15 @@ export async function POST(req: Request){
             return new NextResponse('No image found',{status:400})
         }
 
+        const firebase_url = await uploadFileToFireBase(note[0].imageUrl,note[0].name)
+        await db.update($notes).set({
+            imageUrl: firebase_url
+        }).where(eq($notes.id,parseInt(noteId)))
+
+        return new NextResponse('OK',{status:200})
     } catch (error) {
+        console.log(error)
+        return new NextResponse('Error',{status:500})
         
     }
 }
