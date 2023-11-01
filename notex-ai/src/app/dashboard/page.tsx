e37c4452317c46a750d346cc1a5347e555cb27dc
neeraj-gs <gsneeraj05@gmail.com>
2023-11-01 20:16:28 +0530

    Added a after sign out from dashboard to redirect back to home page

--- notex-ai/src/app/dashboard/page.tsx
+++ notex-ai/src/app/dashboard/page.tsx
@@ -35,7 +35,7 @@ const DashboardPage = async (props: Props) => {
               <div className="w-4"></div>
               <h1 className="text-3xl font-bold text-gray-900">My Notes</h1>
               <div className="w-4"></div>
-              <UserButton />
+              <UserButton afterSignOutUrl="/"/>
             </div>
           </div>
 
