0fa3d0eea2d6821363097b449f69503b037c5877
neeraj-gs <gsneeraj05@gmail.com>
2023-11-01 09:32:44 +0530

    Added the Image descriptoi to dalle to geenrate an image

--- notex-ai/src/lib/openai.ts
+++ notex-ai/src/lib/openai.ts
@@ -1,15 +1,10 @@
 import {Configuration,OpenAIApi} from  'openai-edge'
 
-// const openai = new OpenAIApi({
-//     apiKey: process.env.OPENAI_API_KEY,
-//     engine: 'davinci'
-// })
-
 const config =  new Configuration({
     apiKey: process.env.OPENAI_API_KEY
 })
 
-const openai = new OpenAIApi(config);
+const openai = new OpenAIApi(config)
 
 
 export async function  generateImagePrompt(name:string){
@@ -20,23 +15,18 @@ export async function  generateImagePrompt(name:string){
 
     try{
         const response = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages:[
-            {
-                role:"system",
-                content:"You Are a Creative and Helpful AP Assistant capable  of generating interesting thumbnail descriptions for my notes. Your Output will be Fed to the DALLE API to generaate a thumbnail. The desription should be minimalistic and flat styled",
-            },
-            {
-                role: "user",
-                content: `Please Generate a Thumbnaol description for my notebook titles ${name}`
-            },
-            ],
-
-        });
+            model: 'gpt-3.5-turbo',
+            messages:[{
+                role:'system',
+                content:'You Are a Creative and Helpful AP Assistant capable  of generating interesting thumbnail descriptions for my notes. Your Output will be Fed to the DALLE API to generaate a thumbnail. The desription should be minimalistic and flat styled'
+            },{
+                role: 'user',
+                content: `Please Generate a Thumbnaol description for my notebook title ${name}`
+            }]
+        })
         const data = await response.json();
-        console.log(data);
-        // const image_description = data.choices[0].message.content;
-        // return image_description as string;
+        const image_description = data.choices[0].message.content;
+        return image_description as string;
         
     }catch(error){
         console.log(error);
@@ -46,6 +36,18 @@ export async function  generateImagePrompt(name:string){
 
 }
 
-export async function generateImage(){
+export async function generateImage(image_description:string){
+    try {
+        const response = await openai.createImage({
+          prompt: image_description,
+          n: 1,
+          size: "256x256",
+        });
+        const data = await response.json();
+        const image_url = data.data[0].url;
+        return image_url as string;
+      } catch (error) {
+        console.error(error);
+      }
 
 }
